4a3d7c7b9266c361e53ade3e88c6927e527bd00a
Jeferson Farias Calazans <calazans10@gmail.com>
2013-09-07 09:28:27 -0300

    improve django-mobile-helper

--- django_mobile/middleware.py
+++ django_mobile/middleware.py
@@ -1,43 +1,43 @@
-#coding: utf-8
-
+# -*- coding: utf-8 -*-
 from django.conf import settings
+from constants import AGENT, VERSION
 import re
+
 
 class MobileMiddleware(object):
 
-	def is_mobile_useragent(self,request):
+    ORIG_TEMPLATE_DIRS = settings.TEMPLATE_DIRS
 
-		if request.META.has_key('HTTP_USER_AGENT'):
-			user_agent = request.META['HTTP_USER_AGENT']
+    def is_mobile_useragent(self, request):
+        if 'HTTP_USER_AGENT' in request.META:
+            user_agent = request.META['HTTP_USER_AGENT']
+            reg_b = re.compile(AGENT, re.I | re.M)
+            reg_v = re.compile(VERSION, re.I | re.M)
 
-			pattern = "(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|windows ce|pda|mobile|mini|palm|netfront)"
+            b = reg_b.search(user_agent)
+            v = reg_v.search(user_agent[0:4])
 
-			if hasattr(settings, 'MOBILE_PATTERN'):
-				pattern = settings.MOBILE_PATTERN
+            if b or v:
+                return True
 
-			prog = re.compile(pattern, re.IGNORECASE)
-			match = prog.search(user_agent)
-			return match
+        return False
 
-		return False
+    def process_request(self, request):
+        request.is_mobile = False
+        domain = request.META.get('HTTP_HOST', '').split('.')
 
-	def process_request(self, request):
+        if 'm' in domain or 'mobile' in domain:
+            request.is_mobile = True
+        elif self.is_mobile_useragent(request):
+            request.is_mobile = True
+        else:
+            request.is_mobile = False
 
-		domain = request.META.get('HTTP_HOST', '').split('.')
+        if request.is_mobile:
+            settings.TEMPLATE_DIRS = settings.MOBILE_TEMPLATE_DIRS + \
+                self.ORIG_TEMPLATE_DIRS
+        else:
+            settings.TEMPLATE_DIRS = settings.DESKTOP_TEMPLATE_DIRS + \
+                self.ORIG_TEMPLATE_DIRS
 
-		if 'm' in domain or 'mobile' in domain:
-			request.is_mobile = True
-
-		elif self.is_mobile_useragent(request):
-			request.is_mobile = True
-		else:
-			request.is_mobile = False
-
-		if request.is_mobile:
-			pass
-			#settings.TEMPLATE_DIRS = settings.MOBILE_TEMPLATE_DIRS
-		else:
-			pass
-			#settings.TEMPLATE_DIRS = settings.DESKTOP_TEMPLATE_DIRS
-			
-		return None
+        return None
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
-    name = "django-movel",
-    version = "0.1",
-    packages = find_packages(),
-    author = "Victor Fontes",
-    author_email = "contato@victorfontes.com",
-    description = "A package to help dealing with mobile clients in Django",
-    url = "http://github.com/victorfontes/django-mobile-helper",
-    include_package_data = True
+    name="django-mobile",
+    version="0.2",
+    packages=find_packages(),
+    author="Jeferson Farias Calazans",
+    author_email="calazans10@gmail.com",
+    description="A package to help dealing with mobile clients in Django",
+    url="http://github.com/calazans10/django-mobile-helper",
+    include_package_data=True
 )
